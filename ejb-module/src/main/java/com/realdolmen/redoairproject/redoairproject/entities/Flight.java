4e74e256453483142337590c86907410cd9f9cc8
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-02 10:39:35 +0200

    initial commit

--- ejb-module/src/main/java/com/realdolmen/redoairproject/redoairproject/entities/Flight.java
+++ ejb-module/src/main/java/com/realdolmen/redoairproject/redoairproject/entities/Flight.java
@@ -2,9 +2,6 @@ package com.realdolmen.redoairproject.redoairproject.entities;
 
 import javax.persistence.*;
 
-/**
- * Created by YDEAX41 on 10/09/2015.
- */
 @Entity
 public abstract class Flight extends AbstractEntity {
 
