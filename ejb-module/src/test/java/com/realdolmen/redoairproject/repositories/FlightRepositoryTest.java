58e23ab4c40f008658a9af15c695783cec4c7c58
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-02 14:25:43 +0200

    testing classes added

---
+++ ejb-module/src/test/java/com/realdolmen/redoairproject/repositories/FlightRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.realdolmen.redoairproject.repositories;
+
+import com.realdolmen.redoairproject.redoairproject.entities.Flight;
+import com.realdolmen.redoairproject.redoairproject.repositories.FlightRepository;
+import org.junit.*;
+
+public class FlightRepositoryTest
+{
+
+    private Flight flight;
+    private FlightRepository flightRepository;
+
+    @Before
+    public void before()
+    {
+        //flight = new Flight();
+    }
+
+    @Test
+    public void testFlightPersists()
+    {
+        Assert.fail("To be implemented");
+    }
+}
