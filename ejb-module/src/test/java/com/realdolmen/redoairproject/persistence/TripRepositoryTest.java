2dcc13ec24dc1479924f6ca6113d5c741fc094f0
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-06 19:02:32 +0200

    map works with available countries turning black

--- ejb-module/src/test/java/com/realdolmen/redoairproject/persistence/TripRepositoryTest.java
+++ ejb-module/src/test/java/com/realdolmen/redoairproject/persistence/TripRepositoryTest.java
@@ -1,4 +1,47 @@
 package com.realdolmen.redoairproject.persistence;
 
-public class TripRepositoryTest {
+import com.realdolmen.redoairproject.entities.Country;
+import com.realdolmen.redoairproject.entities.Trip;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class TripRepositoryTest extends PersistenceTest{
+
+    private static final Logger LOG = LoggerFactory.getLogger(TripRepositoryTest.class);
+
+    @Inject
+    private TripRepository tripRepository = new TripRepository();
+
+    private CountryRepository countryRepository = new CountryRepository();
+
+    private Country country;
+
+    @Before
+    public void before()
+    {
+        countryRepository.entityManager = entityManager();
+        tripRepository.entityManager = entityManager();
+        country = countryRepository.findCountryCodeByCountryCaseInsensitive("France");
+    }
+
+    @Test
+    public void testFindTripsByCountry()
+    {
+        List<Trip> allTripsForCountry = tripRepository.findTripsByCountry(country);
+
+        Assert.assertTrue(allTripsForCountry.size() > 0);
+
+        for(Trip t : allTripsForCountry)
+            LOG.debug(t.toString());
+    }
+
+    @Test
+    public void testFind()
+    {
+        Assert.fail("To be implemented");
+    }
 }
