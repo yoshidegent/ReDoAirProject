ce5f0f202636a8109fbaaa08d2f0cdbf43cb8da9
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-07 16:19:18 +0200

    implemented flight seat availability image

---
+++ web-module/src/main/java/com.realdolmen.redoairproject.controller/DestinationsController.java
@@ -0,0 +1,55 @@
+package com.realdolmen.redoairproject.controller;
+
+import com.realdolmen.redoairproject.entities.Country;
+import com.realdolmen.redoairproject.entities.Trip;
+import com.realdolmen.redoairproject.persistence.CountryRepository;
+import com.realdolmen.redoairproject.persistence.TripRepository;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@RequestScoped
+public class DestinationsController {
+
+    @Inject
+    private CountryRepository countryRepository;
+
+    @Inject
+    private TripRepository tripRepository;
+
+
+    private List<Trip> tripsForDestination = new ArrayList<>();
+
+    public List<Trip> retrieveAllTrips()
+    {
+        return tripRepository.findAll();
+    }
+
+    public String getDestinationFromCountry()
+    {
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String countryName = params.get("country");
+
+        Country country = countryRepository.findCountryCodeByCountryCaseInsensitive(countryName);
+        tripsForDestination = tripRepository.findTripsByCountry(country);
+
+        if(country.getCountry() != null && !country.getCountry().equals(""))
+            return country.getCountry();
+        else
+            return "";
+    }
+
+    public List<Trip> getTripsForDestination() {
+        return tripsForDestination;
+    }
+
+    public void setTripsForDestination(List<Trip> tripsForDestination) {
+        this.tripsForDestination = tripsForDestination;
+    }
+}
