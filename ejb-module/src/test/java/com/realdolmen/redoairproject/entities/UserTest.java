1b9c6521522228cbee743761fb468a3f04188a51
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-10 15:10:38 +0200

    improved the User class and tested

--- ejb-module/src/test/java/com/realdolmen/redoairproject/entities/UserTest.java
+++ ejb-module/src/test/java/com/realdolmen/redoairproject/entities/UserTest.java
@@ -5,42 +5,33 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by YDEAX41 on 10/10/2015.
- */
 public class UserTest
 {
     private static final Logger LOG = LoggerFactory.getLogger(User.class);
 
-    private User user;
-
     @Test
-    public void testPasswordIsHashed()
+    public void testPasswordIsHashedAndValid()
     {
         String passwordToBeTested;
-        String hashedPassword;
 
         passwordToBeTested = "password";
-        user = new Passenger("testPassenger", passwordToBeTested);
-        hashedPassword = new String (user.hashPassword(passwordToBeTested));
+        User user = new Passenger("testPassenger", passwordToBeTested);
         //Check if the passwords can becompared after hashing
-        Assert.assertEquals(new String(user.getHashedPassword()), hashedPassword);
+        Assert.assertTrue(user.checkPasswordIsValid(passwordToBeTested));
         //Check if the hashed password is not equal to the original one
-        Assert.assertNotEquals(passwordToBeTested, hashedPassword);
-        LOG.debug("Hashed password: " + hashedPassword);
+        Assert.assertNotEquals(passwordToBeTested, user.getHashedPasswordAsString());
+        LOG.debug("Hashed password: " + user.getHashedPasswordAsString());
 
         passwordToBeTested = "";
         user = new Partner("testPassenger", passwordToBeTested);
-        hashedPassword = new String (user.hashPassword(passwordToBeTested));
-        Assert.assertEquals(new String(user.getHashedPassword()), hashedPassword);
-        Assert.assertNotEquals(passwordToBeTested, hashedPassword);
-        LOG.debug("Hashed password: " + hashedPassword);
+        Assert.assertTrue(user.checkPasswordIsValid(passwordToBeTested));
+        Assert.assertNotEquals(passwordToBeTested, user.getHashedPasswordAsString());
+        LOG.debug("Hashed password: " + user.getHashedPasswordAsString());
 
-        passwordToBeTested = "µùmùµùmdfs^$!!((&é)éà^^^^$$µùù54648431123é2@@@@";
+        passwordToBeTested = "µùmùµùmdfs^$!!((&é)éà^^^^$$µùù54648431123##^{^@@@||@é2@@@@";
         user = new ReDoEmployee("testPassenger", passwordToBeTested);
-        hashedPassword = new String (user.hashPassword(passwordToBeTested));
-        Assert.assertEquals(new String(user.getHashedPassword()), hashedPassword);
-        Assert.assertNotEquals(passwordToBeTested, hashedPassword);
-        LOG.debug("Hashed password: " + hashedPassword);
+        Assert.assertTrue(user.checkPasswordIsValid(passwordToBeTested));
+        Assert.assertNotEquals(passwordToBeTested, user.getHashedPasswordAsString());
+        LOG.debug("Hashed password: " + user.getHashedPasswordAsString());
     }
 }
