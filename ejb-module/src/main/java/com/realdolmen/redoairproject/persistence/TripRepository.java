24712c5fb529badba2043623d1ee4bbc570f446e
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-12 13:59:43 +0200

    added a query for returning only valid trips

--- ejb-module/src/main/java/com/realdolmen/redoairproject/persistence/TripRepository.java
+++ ejb-module/src/main/java/com/realdolmen/redoairproject/persistence/TripRepository.java
@@ -1,5 +1,7 @@
 package com.realdolmen.redoairproject.persistence;
 
+import com.realdolmen.redoairproject.converter.LocalDatePersistenceConverter;
+import com.realdolmen.redoairproject.entities.Flight;
 import com.realdolmen.redoairproject.entities.Trip;
 import com.realdolmen.redoairproject.persistence.interfaces.ITripRepository;
 
@@ -9,25 +11,63 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-@LocalBean
-@Stateless
-public class TripRepository extends GenericRepository<Trip> implements ITripRepository, Serializable {
+@LocalBean @Stateless public class TripRepository extends GenericRepository<Trip>
+    implements ITripRepository, Serializable {
 
     public TripRepository() {
         super(Trip.class);
     }
 
     @Override public List<Trip> findTripsByCountry(Country country) {
-        TypedQuery<Trip> query = entityManager.createQuery("SELECT t FROM Trip t WHERE t.endDestination.address.country.countryCode = :countryCode", Trip.class);
+        TypedQuery<Trip> query = entityManager.createQuery(
+            "SELECT t FROM Trip t WHERE t.endDestination.address.country.countryCode = :countryCode",
+            Trip.class);
         query.setParameter("countryCode", country.getCountryCode());
         return query.getResultList();
     }
 
 
     @Override public List<Country> findAllCountriesFromTrips() {
-        TypedQuery<Country> query = entityManager.createQuery("SELECT t.endDestination.address.country FROM Trip t", Country.class);
+        TypedQuery<Country> query = entityManager
+            .createQuery("SELECT t.endDestination.address.country FROM Trip t WHERE (select count(f.id) from t.flightList f) > 0", Country.class);
         return query.getResultList();
+    }
+
+
+    public List<Trip> findValidTrips(Country country, Date periodStart, Date periodEnd,
+        int numberOfPassengers) {
+
+        List<Trip> tripsByCountry = findTripsByCountry(country);
+
+        List<Trip> resultingTrips = new ArrayList<>();
+
+        for (Trip trip : tripsByCountry) {
+
+            LocalDate beginDate = trip.calculateBeginDate();
+            LocalDate endDate = trip.calculateEndDate();
+
+            LocalDatePersistenceConverter localDatePersistenceConverter =
+                new LocalDatePersistenceConverter();
+
+            LocalDate localBeginDate = localDatePersistenceConverter
+                .convertToEntityAttribute(new java.sql.Date(periodStart.getTime()));
+            LocalDate localEndDate = localDatePersistenceConverter
+                .convertToEntityAttribute(new java.sql.Date(periodEnd.getTime()));
+
+            if (beginDate.isAfter(localBeginDate) && endDate.isBefore(localEndDate)
+                && numberOfPassengers < trip.calculateNumberOfPlacesAvailableForAllFlights()
+                && trip.getFlightList() != null && trip.getFlightList().size() > 0)
+            {
+                resultingTrips.add(trip);
+            }
+        }
+
+        return resultingTrips;
     }
 }
--- ejb-module/src/test/java/com/realdolmen/redoairproject/persistence/TripRepositoryTest.java
+++ ejb-module/src/test/java/com/realdolmen/redoairproject/persistence/TripRepositoryTest.java
@@ -1,13 +1,17 @@
 package com.realdolmen.redoairproject.persistence;
 
+import com.realdolmen.redoairproject.converter.LocalDatePersistenceConverter;
 import com.realdolmen.redoairproject.entities.Country;
+import com.realdolmen.redoairproject.entities.Flight;
 import com.realdolmen.redoairproject.entities.Trip;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
 
 public class TripRepositoryTest extends PersistenceTest{
 
@@ -34,9 +38,12 @@ public class TripRepositoryTest extends PersistenceTest{
         for (Country country1 : countries) {
             List<Trip> tripsByCountry = tripRepository.findTripsByCountry(country1);
             Assert.assertTrue(tripsByCountry.size() > 0);
+            for(Trip t : tripsByCountry)
+            {
+                Assert.assertNotNull(t.getFlightList());
+                Assert.assertTrue(t.getFlightList().size() > 0);
+            }
         }
-
-        entityManager().clear();
     }
 
     @Test
@@ -50,8 +57,6 @@ public class TripRepositoryTest extends PersistenceTest{
 
         for(Trip t : allTripsForCountry)
             LOG.debug(t.toString());
-
-        entityManager().clear();
     }
 
     @Test
@@ -59,12 +64,31 @@ public class TripRepositoryTest extends PersistenceTest{
     {
         List<Trip> allTrips = tripRepository.findAll();
 
-        for(int i=0; i<allTrips.size()-2; i++)
-        {
-            if(allTrips.get(i).getFlightList().size() > 0) {
-                Assert.assertNotNull(allTrips.get(i).getBeginDate());
-                Assert.assertNotNull(allTrips.get(i).getEndDate());
+        for (Trip trip : allTrips) {
+            if (trip.getFlightList().size() > 0) {
+                Assert.assertNotNull(trip.getBeginDate());
+                Assert.assertNotNull(trip.getEndDate());
             }
+        }
+    }
+
+
+    @Test
+    public void testFindValidTrips()
+    {
+        Country country = new Country("GE", "Germany");
+        Date startPeriod = new GregorianCalendar(1900,Calendar.JANUARY,31).getTime();
+        Date endPeriod = new GregorianCalendar(2100,Calendar.JANUARY,31).getTime();
+        int numberOfPassengers = 5;
+
+        List<Trip> allTrips = tripRepository.findValidTrips(country, startPeriod, endPeriod, numberOfPassengers);
+
+        for (Trip t : allTrips)
+        {
+            Assert.assertNotNull(t.getFlightList());
+            Assert.assertTrue(t.getFlightList().size() > 0);
+            Assert.assertTrue(t.getBeginDate().isAfter(new LocalDatePersistenceConverter().convertToEntityAttribute(new java.sql.Date(startPeriod.getTime()))));
+            Assert.assertTrue(t.getEndDate().isBefore(new LocalDatePersistenceConverter().convertToEntityAttribute(new java.sql.Date(endPeriod.getTime()))));
         }
     }
 }
