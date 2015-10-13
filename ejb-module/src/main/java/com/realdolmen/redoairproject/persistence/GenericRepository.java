e573f0acce82f2ce56814555a8752d335bd2ecb9
Evelyne Wauters <evelyne.wauters@gmail.com>
2015-10-13 11:22:35 +0200

    Front End - Update Flight redirects to flight detail page

--- ejb-module/src/main/java/com/realdolmen/redoairproject/persistence/GenericRepository.java
+++ ejb-module/src/main/java/com/realdolmen/redoairproject/persistence/GenericRepository.java
@@ -36,7 +36,6 @@ public abstract class GenericRepository<T extends Serializable> implements IGene
     public T createOrUpdate(T t)
     {
         T t2 = entityManager.merge(t);
-//        entityManager.flush();
         return t2;
     }
 
